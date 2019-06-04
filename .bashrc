81639e125d02e37471005da510b80e4b88b61168
root <root@localhost.localdomain>
2019-06-05 00:05:04 +0200

    added alacrit as terminal - plus vmware fuse mount hack simply in .xinitrc

--- .bashrc
+++ .bashrc
@@ -7,11 +7,15 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias pin='ping 8.8.8.8'
 alias gs='git status'
+alias wd='display_size w'
+alias wm='display_size m'
 # Source global definitions
 if [ -f /etc/bashrc ]; then
 	. /etc/bashrc
 fi
 
-
+ff () {
+     find . -type f -type d -name proc -prune -o -name "*" | grep --color=auto -i "$1"
+}
 
 export PATH="/root/bin:$PATH"
--- inst/suckless/dwm-6.2/config.h
+++ inst/suckless/dwm-6.2/config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox.disabled.gk",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -60,7 +60,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+/*static const char *termcmd[]  = { "/usr/local/bin/st", NULL }; */
+static const char *termcmd[]  = { "/usr/bin/alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
