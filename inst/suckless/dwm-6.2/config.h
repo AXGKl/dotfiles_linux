28c969cc2c7be834eb682229ea6fa75d330c7bfc
guntherklessinger <root@localhost.localdomain>
2019-06-05 02:09:52 +0200

    removed binaries - clipboard via alt-super-c

--- inst/suckless/dwm-6.2/config.h
+++ inst/suckless/dwm-6.2/config.h
@@ -67,7 +67,7 @@ static const char *clipcmd[]  = { "/root/bin/keratin", "-nb", col_gray1, "-nf", 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ControlMask,           XK_c,      spawn,          {.v = clipcmd } },
+	{ MODKEY|Mod1Mask,           XK_c,      spawn,          {.v = clipcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
